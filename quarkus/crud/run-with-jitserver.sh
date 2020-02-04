2d6fab047e426d444d3d6227d5ab3e0384e59883
Mark Stoodley <mstoodle@ca.ibm.com>
2020-02-04 12:48:18 -0500

    Add core configuration options to run-with-jitserver.sh
    Take options to specify cores for JIT server to use as well as for the application server to use.

--- quarkus/crud/run-with-jitserver.sh
+++ quarkus/crud/run-with-jitserver.sh
@@ -1,16 +1,18 @@
 #/bin/bash -f
 
-if [[ $# < 3 ]]; then
-	echo "Usage: $0 <#runs> <jdk8 or jdk11> <variant used in image names>"
+if [[ $# < 5 ]]; then
+	echo "Usage: $0 <#runs> <JIT cores> <appserver cores> <jdk8 or jdk11> <variant used in image names>"
 	exit -1
 fi
 
 RUNS=$1
-JDK=$2
-VARIANT=$3
+JIT_CORES=$2
+APPSERVER_CORES=$3
+JDK=$4
+VARIANT=$5
 
-sudo docker run -d --name jitserv-${JDK} -t --cpuset-cpus="16-23" --rm -p 38400:38400 --network host jitserver-${JDK}
+sudo docker run -d --name jitserv-${JDK} -t --cpuset-cpus=$JIT_CORES --rm -p 38400:38400 --network host jitserver-${JDK}
 
-./run-server.sh ${RUNS} ${JDK} ${VARIANT}
+./run-server.sh ${RUNS} ${APPSERVER_CORES} ${JDK} ${VARIANT}
 
 sudo docker stop jitserv-${JDK}
