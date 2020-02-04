ba181704ffb48f73bb2bbd9c55e9a0aa20ef8a49
Mark Stoodley <mstoodle@ca.ibm.com>
2020-02-04 12:37:56 -0500

    Add verification to build-check.sh
    Should built 19 images, so actually check that we built 19 images successfully

--- quarkus/crud/build-check.sh
+++ quarkus/crud/build-check.sh
@@ -4,3 +4,9 @@ if [[ $# < 1 ]]; then
 	exit -1
 fi
 grep 'Successfully tagged' $1
+NUMSUCCESS=`grep -c 'Successfully tagged' $1`
+if [[ $NUMSUCCESS == 19 ]]; then
+	echo All images sucessfully built!
+else
+	echo Should be 19 images, only got $NUMSUCCESS
+fi
