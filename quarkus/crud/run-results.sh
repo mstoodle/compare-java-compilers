db2af5eccec761d0530ce0574aa59e575ce9784d
Mark Stoodley <mstoodle@ca.ibm.com>
2020-02-04 12:54:26 -0500

    Specify JIT and application server cores in runs
    Configure application server to run on one core (core 0) and the JIT server to use a different 3 cores (1-3).

--- quarkus/crud/run-results.sh
+++ quarkus/crud/run-results.sh
@@ -18,24 +18,24 @@ mkdir runs
 
 ./run-docker-db.sh
 
-./run-server.sh         $RUNS jdk8 native
-./run-server.sh         $RUNS jdk8 native-tuned
-./run-server.sh         $RUNS jdk8 hs
-./run-server.sh         $RUNS jdk8 j9
-./run-server.sh         $RUNS jdk8 j9-sc
-./run-server.sh         $RUNS jdk8 j9-scvirt
-./run-server.sh         $RUNS jdk11 hs
-./run-server.sh         $RUNS jdk11 hs-jaotc
-./run-server.sh         $RUNS jdk11 hs-jaotc-tier
-./run-server.sh         $RUNS jdk11 graal
-./run-server.sh         $RUNS jdk11 j9
-./run-server.sh         $RUNS jdk11 j9-sc
-./run-server.sh         $RUNS jdk11 j9-scvirt
+./run-server.sh         $RUNS 0 jdk8 native
+./run-server.sh         $RUNS 0 jdk8 native-tuned
+./run-server.sh         $RUNS 0 jdk8 hs
+./run-server.sh         $RUNS 0 jdk8 j9
+./run-server.sh         $RUNS 0 jdk8 j9-sc
+./run-server.sh         $RUNS 0 jdk8 j9-scvirt
+./run-server.sh         $RUNS 0 jdk11 hs
+./run-server.sh         $RUNS 0 jdk11 hs-jaotc
+./run-server.sh         $RUNS 0 jdk11 hs-jaotc-tier
+./run-server.sh         $RUNS 0 jdk11 graal
+./run-server.sh         $RUNS 0 jdk11 j9
+./run-server.sh         $RUNS 0 jdk11 j9-sc
+./run-server.sh         $RUNS 0 jdk11 j9-scvirt
 
-./run-with-jitserver.sh $RUNS jdk8 j9-jitserver
-./run-with-jitserver.sh $RUNS jdk8 j9-sc-jitserver
-./run-with-jitserver.sh $RUNS jdk11 j9-jitserver
-./run-with-jitserver.sh $RUNS jdk11 j9-sc-jitserver
+./run-with-jitserver.sh $RUNS 1-3 0 jdk8 j9-jitserver
+./run-with-jitserver.sh $RUNS 1-3 0 jdk8 j9-sc-jitserver
+./run-with-jitserver.sh $RUNS 1-3 0 jdk11 j9-jitserver
+./run-with-jitserver.sh $RUNS 1-3 0 jdk11 j9-sc-jitserver
 
 ./extract-su.awk runs/out.* > runs/all.su.txt
 ./extract-fp.awk runs/fp.* > runs/all.fp.txt
