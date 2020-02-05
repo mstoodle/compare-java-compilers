0a7a27350cc22194e0b02cb96f8eb7ce0a0b58c4
Mark Stoodley <mstoodle@ca.ibm.com>
2020-02-05 16:20:23 -0500

    Refactor run-results.sh and store multi run results independently
    Refactor most of the runs into do_all_runs() function which can be more easily
    changed to do more kinds of runs (on different numbers of cores, say). Provide
    some examples (commented out) for running 4-core servers (on at least an 8-core
    machine) and 24-core servers (on at least a 32-core machine).
    
    Also summarize data per run.

--- quarkus/crud/run-results.sh
+++ quarkus/crud/run-results.sh
@@ -5,10 +5,31 @@ if [[ "$1" == "--build" ]]; then
 	shift
 fi
 
-RUNS=5
-if [[ $1 > 0 ]]; then
-        RUNS=$1
-fi
+do-all-runs() {
+	RUNS=$1
+        JITCORES=$2
+        APPCORES=$3
+
+        echo "Runs with APPCORES=$APPCORES and JITCORES=$JITCORES"
+        ./run-server.sh         $RUNS $APPCORES jdk8 native
+        ./run-server.sh         $RUNS $APPCORES jdk8 native-tuned
+        ./run-server.sh         $RUNS $APPCORES jdk8 hs
+        ./run-server.sh         $RUNS $APPCORES jdk8 j9
+        ./run-server.sh         $RUNS $APPCORES jdk8 j9-sc
+        ./run-server.sh         $RUNS $APPCORES jdk8 j9-scvirt
+        ./run-server.sh         $RUNS $APPCORES jdk11 hs
+        ./run-server.sh         $RUNS $APPCORES jdk11 hs-jaotc
+        ./run-server.sh         $RUNS $APPCORES jdk11 hs-jaotc-tier
+        ./run-server.sh         $RUNS $APPCORES jdk11 graal
+        ./run-server.sh         $RUNS $APPCORES jdk11 j9
+        ./run-server.sh         $RUNS $APPCORES jdk11 j9-sc
+        ./run-server.sh         $RUNS $APPCORES jdk11 j9-scvirt
+
+        ./run-with-jitserver.sh $RUNS $JITCORES $APPCORES jdk8 j9-jitserver
+        ./run-with-jitserver.sh $RUNS $JITCORES $APPCORES jdk8 j9-sc-jitserver
+        ./run-with-jitserver.sh $RUNS $JITCORES $APPCORES jdk11 j9-jitserver
+        ./run-with-jitserver.sh $RUNS $JITCORES $APPCORES jdk11 j9-sc-jitserver
+}
 
 # Give 2 chances for foolishness
 rm -rf runs-old2 > /dev/null
@@ -18,31 +39,30 @@ mkdir runs
 
 ./run-docker-db.sh
 
+RUNS=5
+if [[ $1 > 0 ]]; then
+        RUNS=$1
+fi
+
+# Do single core runs on at least a 4-core machine
 APPCORES="0"
 JITCORES="1-3"
-echo Runs with APPCORES=$APPCORES and JITCORES=$JITCORES
+do-all-runs.sh $RUNS $JITCORES $APPCORES
 
-./run-server.sh         $RUNS $APPCORES jdk8 native
-./run-server.sh         $RUNS $APPCORES jdk8 native-tuned
-./run-server.sh         $RUNS $APPCORES jdk8 hs
-./run-server.sh         $RUNS $APPCORES jdk8 j9
-./run-server.sh         $RUNS $APPCORES jdk8 j9-sc
-./run-server.sh         $RUNS $APPCORES jdk8 j9-scvirt
-./run-server.sh         $RUNS $APPCORES jdk11 hs
-./run-server.sh         $RUNS $APPCORES jdk11 hs-jaotc
-./run-server.sh         $RUNS $APPCORES jdk11 hs-jaotc-tier
-./run-server.sh         $RUNS $APPCORES jdk11 graal
-./run-server.sh         $RUNS $APPCORES jdk11 j9
-./run-server.sh         $RUNS $APPCORES jdk11 j9-sc
-./run-server.sh         $RUNS $APPCORES jdk11 j9-scvirt
+# Uncomment to do 4 core runs on at least an 8-core machine
+#APPCORES="4-7"
+#JITCORES="0-3"
+#do-all-runs.sh $RUNS $JITCORES $APPCORES
 
-./run-with-jitserver.sh $RUNS $JITCORES $APPCORES jdk8 j9-jitserver
-./run-with-jitserver.sh $RUNS $JITCORES $APPCORES jdk8 j9-sc-jitserver
-./run-with-jitserver.sh $RUNS $JITCORES $APPCORES jdk11 j9-jitserver
-./run-with-jitserver.sh $RUNS $JITCORES $APPCORES jdk11 j9-sc-jitserver
+# Uncomment next 3 lines to do 24 core runs on at least a 32-core machine
+#APPCORES="0-23"
+#JITCORES="24-31"
+#do-all-runs.sh $RUNS $JITCORES $APPCORES
 
 ./stop-docker-db.sh
 
-./extract.su.awk runs/out.* > runs/all.su.txt
-./extract.fp.awk runs/fp.*  > runs/all.fp.txt
-./extract.tp.awk runs/tp.*  > runs/all.tp.txt
+for r in $RUNS; do
+	./extract.su.awk runs/out.*-$r > runs/all.su.$r.txt
+	./extract.fp.awk runs/fp.*-$r  > runs/all.fp.$r.txt
+	./extract.tp.awk runs/tp.*-$r  > runs/all.tp.$r.txt
+done
